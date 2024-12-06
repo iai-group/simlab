221197092ca1cd2a49a925d1e5ef5e6fc64781cd
Nolwenn <28621493+NoB0@users.noreply.github.com>
2024-12-06 12:03:41 -0600

    Add tutorials

---
+++ webapp/frontend/src/components/docs/DocIndex.tsx
@@ -0,0 +1,39 @@
+// Documentation index
+
+const DocIndex = () => {
+  return (
+    <>
+      <h3>Documentation</h3>
+
+      <div>
+        <p>
+          SimLab is a platform for performing simulation-based evaluation of
+          conversational systems. It aims to:
+        </p>
+        <ul>
+          <li>Provide a unified interface.</li>
+          <li>Centralize the resources.</li>
+          <li>Facilitate the reproducibility of experiments.</li>
+          <li>Enable the comparison of different systems.</li>
+        </ul>
+
+        <p>
+          The documentation provides information on the available resources
+          (e.g., tasks, metrics, and systems) and tutorials to help you get
+          started with the platform.
+        </p>
+
+        <p>
+          <b>Pre-requisites:</b>
+          <ul>
+            <li>Docker installed on your machine.</li>
+            <li>Basic knowledge of Docker.</li>
+            <li>Basic knowledge of REST APIs.</li>
+          </ul>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default DocIndex;
