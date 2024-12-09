88fecbe8ed759e07e86ad9303dfc1060df62855b
Nolwenn <28621493+NoB0@users.noreply.github.com>
2024-12-09 15:29:12 -0600

    Add tests for run related routes

--- tests/backend/__init__.py
+++ tests/backend/__init__.py
@@ -11,10 +11,72 @@ dummy_metrics = [
     {"name": "success_rate", "description": "Success rate"},
     {"name": "bleu", "description": "BLEU score"},
 ]
-insert_records(mongo_connector, "metrics", dummy_metrics)
+metrics_ids = insert_records(mongo_connector, "metrics", dummy_metrics)
 
 dummy_tasks = [
     {"name": "crs", "description": "CRS Evaluation"},
     {"name": "css", "description": "CSS Evaluation"},
 ]
-insert_records(mongo_connector, "tasks", dummy_tasks)
+task_ids = insert_records(mongo_connector, "tasks", dummy_tasks)
+
+dummy_runs = [
+    {
+        "username": "test_user",
+        "run_name": "test_run",
+        "run_configuration": {
+            "task": {
+                "_id": task_ids[0],
+                "name": "crs",
+                "arguments": {
+                    "n_simulations": 100,
+                },
+            },
+            "agents": [
+                {
+                    "name": "agent",
+                }
+            ],
+            "user_simulators": [
+                {
+                    "name": "user_simulator",
+                }
+            ],
+            "metrics": [
+                {
+                    "_id": metrics_ids[0],
+                    "name": "success_rate",
+                }
+            ],
+        },
+    },
+    {
+        "username": "test_user",
+        "run_name": "test_run2",
+        "run_configuration": {
+            "task": {
+                "_id": task_ids[1],
+                "name": "css",
+                "arguments": {
+                    "n_simulations": 100,
+                },
+            },
+            "agents": [
+                {
+                    "name": "agent",
+                }
+            ],
+            "user_simulators": [
+                {
+                    "name": "user_simulator",
+                }
+            ],
+            "metrics": [
+                {
+                    "_id": metrics_ids[0],
+                    "name": "success_rate",
+                }
+            ],
+        },
+    },
+]
+insert_records(mongo_connector, "runs", dummy_runs)
