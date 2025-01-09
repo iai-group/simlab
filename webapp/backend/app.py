11cd91a9acf1eab9ce95722e2c1de7a8dbeee7ee
sharath07chandra <sharath07chandra@yahoo.in>
2025-01-09 00:31:33 -0600

    Jenkins initial setup

--- webapp/backend/app.py
+++ webapp/backend/app.py
@@ -8,10 +8,12 @@ from flask_login import LoginManager
 from connectors.docker.docker_registry_connector import DockerRegistryConnector
 from connectors.mongo.mongo_connector import MongoDBConnector
 from connectors.mongo.user import User
+from connectors.jenkins.jenkins_vm_manager import JenkinsVMManager
 
 login_manager = LoginManager()
 mongo_connector = MongoDBConnector()
 docker_registry_connector = DockerRegistryConnector()
+jenkins_vm = JenkinsVMManager()
 
 
 @login_manager.user_loader
@@ -76,6 +78,8 @@ def create_app(testing: bool = False) -> Flask:
 
     if testing:
         mongo_connector.set_default_db("simlab_test")
+    
+    jenkins_vm.check_jenkins_connection()
 
     # Register blueprints
     from webapp.backend.routes.auth import auth as auth_blueprint
